b544a304a8c78d1ee4336ac8f4f34decfc621e8d
ilyaDubovtsev <dubovtsev98@gmail.com>
2020-02-24 23:17:24 +0400

    add url process by goroutines

--- main.go
+++ main.go
@@ -4,12 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sync"
 )
 
+type urlWork struct{ url string }
+
+func getCountOfGo(url string) int {
+	return 5
+}
+
+//echo -e 'https://golang.org\nhttps://golang.org' | go run main.go
 func main() {
+	works := make(chan urlWork)
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Println('1')
-		fmt.Println(scanner.Text())
+	maxConcurrency := 5
+	wg := new(sync.WaitGroup)
+
+	wg.Add(maxConcurrency)
+	for i := 0; i < maxConcurrency; i++ {
+		go func(j int) {
+			for work := range works {
+				fmt.Println("Count for", work.url, ":", getCountOfGo(work.url), "goroutine", j)
+			}
+			wg.Done()
+		}(i)
+
 	}
+
+	for scanner.Scan() {
+		scannedURL := scanner.Text()
+		fmt.Println("read ", scannedURL)
+		works <- urlWork{scannedURL}
+	}
+
+	close(works)
+	wg.Wait()
 }
